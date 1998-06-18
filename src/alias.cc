ea8effa8615a57b4e1646bff0da6f3b9c1957d1d
lav <>
1998-06-18 14:16:50 +0000

    Initial revision

---
+++ src/alias.cc
@@ -0,0 +1,154 @@
+/*
+ * lftp and utils
+ *
+ * Copyright (c) 1996-1997 by Alexander V. Lukyanov (lav@yars.free.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id$ */
+
+#include <config.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "alias.h"
+#include "xmalloc.h"
+
+Alias *Alias::base;
+
+Alias::Alias(const char *alias,const char *value,Alias *next)
+{
+   this->alias=xstrdup(alias);
+   this->value=xstrdup(value);
+   this->next=next;
+}
+
+Alias::~Alias()
+{
+   free(this->alias);
+   free(this->value);
+}
+
+void Alias::Add(const char *alias,const char *value)
+{
+   Alias **scan=&base;
+   while(*scan)
+   {
+      int dif=strcmp((*scan)->alias,alias);
+      if(dif==0)
+      {
+	 free((*scan)->value);
+	 (*scan)->value=xstrdup(value);
+	 return;
+      }
+      if(dif>0)
+	 break;
+      scan=&((*scan)->next);
+   }
+   *scan=new Alias(alias,value,*scan);
+}
+
+void Alias::Del(const char *alias)
+{
+   Alias **scan=&base;
+   while(*scan)
+   {
+      int dif=strcmp((*scan)->alias,alias);
+      if(dif==0)
+      {
+	 Alias *tmp=(*scan)->next;
+	 delete *scan;
+	 *scan=tmp;
+	 return;
+      }
+      scan=&((*scan)->next);
+   }
+}
+
+const char *Alias::Find(const char *alias)
+{
+   Alias *scan=base;
+   while(scan)
+   {
+      int dif=strcmp(scan->alias,alias);
+      if(dif==0)
+	 return(scan->value);
+      if(dif>0)
+	 break;
+      scan=scan->next;
+   }
+   return 0;
+}
+
+#if 0
+void Alias::List()
+{
+   Alias *scan=base;
+   while(scan)
+   {
+      printf("alias %s \"%s\"\n",scan->alias,scan->value);
+      scan=scan->next;
+   }
+}
+#endif
+
+char *Alias::Format()
+{
+   char *res;
+   Alias *scan;
+   int size=0;
+
+   for(scan=base; scan; scan=scan->next)
+   {
+      size+=6+strlen(scan->alias)*2;
+      size+=2+strlen(scan->value)*2+1+1;
+   }
+   res=(char*)xmalloc(size+1);
+   char *store=res;
+
+   for(scan=base; scan; scan=scan->next)
+   {
+      strcpy(store,"alias ");
+      store+=strlen(store);
+      char *s=scan->alias;
+      while(*s)
+      {
+	 if(strchr("\" \t\\>|",*s))
+	    *store++='\\';
+	 *store++=*s++;
+      }
+      *store++=' ';
+      s=scan->value;
+
+      bool par=false;
+      if(*s==0 || strcspn(s," \t>|")!=strlen(s))
+	 par=true;
+      if(par)
+	 *store++='"';
+      while(*s)
+      {
+	 if(strchr("\"\\",*s))
+	    *store++='\\';
+	 *store++=*s++;
+      }
+      if(par)
+	 *store++='"';
+      *store++='\n';
+   }
+   *store=0;
+   return res;
+}
---
+++ src/alias.h
@@ -0,0 +1,86 @@
+/*
+ * lftp and utils
+ *
+ * Copyright (c) 1996-1997 by Alexander V. Lukyanov (lav@yars.free.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id$ */
+
+#ifndef ALIAS_H
+#define ALIAS_H
+
+#include "xmalloc.h"
+
+class Alias
+{
+   Alias *next;
+   char	 *alias;
+   char	 *value;
+
+   static Alias *base;
+
+   Alias(const char *alias,const char *value,Alias *next);
+
+public:
+   ~Alias();
+
+   static const char *Find(const char *alias);
+
+   static void Add(const char *alias,const char *value);
+   static void Del(const char *alias);
+
+   static char *Format();
+
+   friend char *command_generator(char *text,int state);
+};
+
+class TouchedAlias
+{
+   char *alias;
+   TouchedAlias *next;
+public:
+   TouchedAlias(const char *a,TouchedAlias *n)
+   {
+      alias=xstrdup(a);
+      next=n;
+   }
+   ~TouchedAlias()
+   {
+      free(alias);
+   }
+   static void FreeChain(TouchedAlias *chain)
+   {
+      while(chain)
+      {
+	 TouchedAlias *next=chain->next;
+	 delete chain;
+	 chain=next;
+      }
+   }
+   static bool IsTouched(const char *a,TouchedAlias *chain)
+   {
+      while(chain)
+      {
+	 if(!strcmp(chain->alias,a))
+	    return true;
+	 chain=chain->next;
+      }
+      return false;
+   }
+};
+
+#endif//ALIAS_H
