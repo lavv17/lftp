743da1b0a5d13ddf960daae72353669113c00d1f
lav <>
1998-08-14 16:05:23 +0000

    Initial revision

---
+++ src/fg.cc
@@ -0,0 +1,74 @@
+/*
+ * lftp and utils
+ *
+ * Copyright (c) 1996-1997 by Alexander V. Lukyanov (lav@yars.free.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id$ */
+
+#include <config.h>
+#include <signal.h>
+#include <unistd.h>
+#include "fg.h"
+
+void FgData::cont()
+{
+   if(!pg)
+      return;
+   if(kill(-pg,SIGCONT)==-1)
+      kill(pg,SIGCONT);
+}
+
+FgData::FgData(pid_t npg,bool fg)
+{
+   old_pgrp=0;
+   pg=npg;
+   if(!pg)
+      return;
+   if(fg)
+      Fg();
+   else
+      cont();
+}
+
+FgData::~FgData()
+{
+   if(old_pgrp)
+      Bg();
+}
+
+void FgData::Fg()
+{
+   if(!pg)
+      return;
+   pid_t tc_grp=tcgetpgrp(0);
+   if(tc_grp==-1 || tc_grp==getpgrp())
+   {
+      old_pgrp=getpgrp();
+      tcsetpgrp(0,pg);
+   }
+   cont();
+}
+
+void FgData::Bg()
+{
+   if(old_pgrp)
+   {
+      tcsetpgrp(0,old_pgrp);
+      old_pgrp=0;
+   }
+}
