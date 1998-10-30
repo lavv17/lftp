fa655f8433cc7d23625b5f9c632f8db201f82857
lav <>
1998-10-30 10:35:59 +0000

    Initial revision

---
+++ src/PollVec.cc
@@ -0,0 +1,137 @@
+/*
+ * lftp and utils
+ *
+ * Copyright (c) 1996-1997 by Alexander V. Lukyanov (lav@yars.free.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <config.h>
+#include <stdio.h>
+#include <assert.h>
+#include "PollVec.h"
+#include "xalloca.h"
+
+void  PollVec::Empty()
+{
+   Waiting *scan,*next;
+   for(scan=chain; scan; scan=next)
+   {
+      next=scan->next;
+      delete scan;
+   }
+   chain=0;
+}
+
+Waiting *Waiting::DupChain()
+{
+   Waiting *new_chain;
+   Waiting **new_ptr=&new_chain;
+   Waiting *w=this;
+
+   while(w)
+   {
+      *new_ptr=new Waiting(*w);
+      new_ptr=&((*new_ptr)->next);
+      w=w->next;
+   }
+   *new_ptr=0;
+   return new_chain;
+}
+
+void  PollVec::Merge(const PollVec& p)
+{
+   Waiting **scan;
+   for(scan=&chain; *scan; scan=&((*scan)->next));
+   *scan=p.chain->DupChain();
+}
+
+PollVec::PollVec(Waiting::Type t)
+{
+   chain=new Waiting;
+   chain->wait_type=t;
+   chain->next=0;
+}
+
+PollVec::PollVec(int fd,int ev)
+{
+   assert(fd>=0);
+   chain=new Waiting;
+   chain->wait_type=Waiting::POLLFD;
+   chain->next=0;
+   chain->pfd.fd=fd;
+   chain->pfd.events=ev;
+}
+
+PollVec::PollVec(int t)
+{
+   chain=new Waiting;
+   chain->wait_type=Waiting::TIMEOUT;
+   chain->next=0;
+   chain->timeout=t;
+}
+
+void  PollVec::Block() const
+{
+   int nfd=0;
+   Waiting *scan;
+   int	 cur_timeout=-1;
+   int	 async=0;
+
+   for(scan=chain; scan; scan=scan->next)
+   {
+      switch(scan->wait_type)
+      {
+      case(Waiting::POLLFD):
+	 nfd++;
+	 break;
+      case(Waiting::TIMEOUT):
+	 if(cur_timeout>scan->timeout || cur_timeout==-1)
+	 {
+	    cur_timeout=scan->timeout;
+	    if(cur_timeout==0)
+	       return;
+	 }
+	 break;
+      case(Waiting::ASYNCWAIT):
+	 async++;
+	 break;
+      case(Waiting::NOWAIT):
+	 return;
+      }
+   }
+
+   if(nfd==0)
+   {
+      if(async==0 && cur_timeout==-1)
+      {
+	 /* dead lock */
+	 fprintf(stderr,"%s: deadlock detected\n","PollVec::Block");
+      	 cur_timeout=1000;
+      }
+      poll(0,0,cur_timeout);
+      return;
+   }
+
+   pollfd *pfd=(pollfd*)alloca(nfd*sizeof(pollfd));
+   int i;
+
+   for(i=0,scan=chain; scan; scan=scan->next)
+      if(scan->wait_type==Waiting::POLLFD)
+	 pfd[i++]=scan->pfd;
+
+   poll(pfd,nfd,cur_timeout);
+   return;
+}
---
+++ src/PollVec.h
@@ -0,0 +1,83 @@
+/*
+ * lftp and utils
+ *
+ * Copyright (c) 1996-1997 by Alexander V. Lukyanov (lav@yars.free.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef POLLVEC_H
+#define POLLVEC_H
+
+#include <sys/types.h>
+
+#ifdef HAVE_SYS_STROPTS_H
+# include <sys/stropts.h>
+#endif
+
+#ifdef HAVE_SYS_POLL_H
+# include <sys/poll.h>
+#else
+# include <poll.h>
+#endif
+
+class SMTask;
+
+class Waiting
+{
+public:
+   enum	Type { POLLFD,TIMEOUT,ASYNCWAIT,NOWAIT };
+
+private:
+   friend class PollVec;
+   Type	    wait_type;
+   struct pollfd pfd;
+   int	    timeout;
+   Waiting  *next;
+
+   Waiting  *DupChain();
+};
+
+class PollVec
+{
+   Waiting  *chain;
+public:
+   PollVec(int fd,int events);
+   PollVec(int timeout);
+   PollVec(SMTask *);
+   PollVec(Waiting::Type);
+   PollVec() { chain=0; }
+
+   void	 Empty();
+   void	 Merge(const PollVec&);
+   void	 Block() const;
+   void	 operator+=(const PollVec& p) { Merge(p); }
+
+   ~PollVec() { Empty(); }
+};
+
+class TimeOut : public PollVec
+{
+public:
+   TimeOut(int t) : PollVec(t) {}
+};
+
+class NoWait : public PollVec
+{
+public:
+   NoWait() : PollVec(Waiting::NOWAIT) {}
+};
+
+#endif /* POLLVEC_H */
