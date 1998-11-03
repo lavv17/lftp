7e6dff3e8e9d9691beb9aaabe07f8f59d35e091c
lav <>
1998-11-03 11:46:00 +0000

    lftp-990611

---
+++ include/xstring.h
@@ -0,0 +1,64 @@
+/*
+ * lftp and utils
+ *
+ * Copyright (c) 1996-1997 by Alexander V. Lukyanov (lav@yars.free.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef XSTRING_H
+#define XSTRING_H
+
+/* Declare string and memory handling routines.  Take care that an ANSI
+   string.h and pre-ANSI memory.h might conflict, and that memory.h and
+   strings.h conflict on some systems.  */
+
+#if STDC_HEADERS || HAVE_STRING_H
+# include <string.h>
+# if !STDC_HEADERS && HAVE_MEMORY_H
+#  include <memory.h>
+# endif
+#else
+# include <strings.h>
+# ifndef strchr
+#  define strchr index
+# endif
+# ifndef strrchr
+#  define strrchr rindex
+# endif
+# ifndef memcpy
+#  define memcpy(Dst, Src, Num) bcopy (Src, Dst, Num)
+# endif
+# ifndef memcmp
+#  define memcmp(Src1, Src2, Num) bcmp (Src1, Src2, Num)
+# endif
+#endif
+
+static inline int xstrcmp(const char *s1,const char *s2)
+{
+   if(s1==s2)
+      return 0;
+   if(s1==0 || s2==0)
+      return 1;
+   return strcmp(s1,s2);
+}
+static inline int xstrlen(const char *s)
+{
+   if(s==0)
+      return 0;
+   return strlen(s);
+}
+
+#endif//XSTRING_H
