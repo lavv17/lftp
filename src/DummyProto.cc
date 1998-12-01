ae7f6e423f9f6537ee0e76d74cf4d06fe8f88bea
lav <>
1998-12-01 11:25:45 +0000

    Initial revision

---
+++ src/DummyProto.cc
@@ -0,0 +1,33 @@
+/*
+ * lftp and utils
+ *
+ * Copyright (c) 1998 by Alexander V. Lukyanov (lav@yars.free.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id$ */
+
+#include <config.h>
+
+#include "DummyProto.h"
+
+int DummyProto::Do() { return STALL; }
+int DummyProto::Done() { return NO_HOST; }
+const char *DummyProto::GetProto() { return ""; }
+FileAccess *DummyProto::Clone() { return new DummyProto; }
+int DummyProto::Read(void *buf,int size) { return NO_HOST; };
+int DummyProto::Write(const void *buf,int size) { return NO_HOST; };
+int DummyProto::StoreStatus() { return NO_HOST; }
