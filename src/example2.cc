6ddafec2af3543476023efdc09930c3eb807d2d9
lav <>
1999-06-14 11:55:15 +0000

    add CVS:

---
+++ src/example2.cc
@@ -0,0 +1,44 @@
+#include <config.h>
+#include "Http.h"
+#include "log.h"
+#include "ResMgr.h"
+#include "SignalHook.h"
+#include <unistd.h>
+
+int main()
+{
+   ResMgr::ClassInit();
+   SignalHook::ClassInit();
+
+   Log::global=new Log();
+   Log::global->SetOutput(2,false);
+   Log::global->SetLevel(5);
+   Log::global->Enable();
+
+   Http f;
+   f.Connect("ftp.yars.free.net",0);
+   f.Open("/pub/software/",f.RETRIEVE);
+   for(;;)
+   {
+      SMTask::Schedule();
+
+      char buf[1024];
+      int res=f.Read(buf,sizeof(buf));
+      if(res<0)
+      {
+	 if(res==f.DO_AGAIN)
+	    continue;
+	 fprintf(stderr,"Error: %s\n",f.StrError(res));
+	 return 1;
+      }
+      if(res==0) // eof
+      {
+	 f.Close();
+	 return 0;
+      }
+      write(1,buf,res);
+
+      SMTask::Block();
+   }
+   return 0;
+}
