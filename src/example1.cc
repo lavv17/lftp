a24b3475e2a07ae1a2961b61334a1c13bdc19d92
lav <>
1999-06-14 23:04:58 +0000

    make it work with latest Ftp

--- src/example1.cc
+++ src/example1.cc
@@ -5,31 +5,37 @@
 
 #include <config.h>
 #include "ftpclass.h"
+#include "ResMgr.h"
+#include "SignalHook.h"
 #include <unistd.h>
 
 int main()
 {
-	Ftp f;
-	f.Connect("ftp.yars.free.net",0);
-	f.Open("/pub/software/unix/net/ftp/client",f.LONG_LIST);
-	for(;;)
-	{
-		f.Block(); // this functions does block until data available,
-				// maybe earlier.
-		char buf[1024];
-		int res=f.Read(buf,sizeof(buf));
-		if(res<0)
-		{
-			if(res==f.DO_AGAIN)
-				continue;
-			fprintf(stderr,"Error: %s\n",f.StrError(res));
-			return 1;
-		}
-		if(res==0) // eof
-		{
-			f.Close();
-			return 0;
-		}
-		write(1,buf,res);
-	}
+   ResMgr::ClassInit();
+   SignalHook::ClassInit();
+
+   Ftp f;
+   f.Connect("ftp.yars.free.net",0);
+   f.Open("/pub/software/unix/net/ftp/client/lftp",f.LONG_LIST);
+   for(;;)
+   {
+      SMTask::Schedule();
+      SMTask::Block();
+
+      char buf[1024];
+      int res=f.Read(buf,sizeof(buf));
+      if(res<0)
+      {
+	 if(res==f.DO_AGAIN)
+	    continue;
+	 fprintf(stderr,"Error: %s\n",f.StrError(res));
+	 return 1;
+      }
+      if(res==0) // eof
+      {
+	 f.Close();
+	 return 0;
+      }
+      write(1,buf,res);
+   }
 }
