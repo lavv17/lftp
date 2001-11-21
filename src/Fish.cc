e6227dd3ae3c68cb6433c637728b8b9b8eacc710
lav <>
2001-11-21 16:43:40 +0000

    * Fish.cc: set user, group, nlinks and mode in FileInfo class.

--- src/Fish.cc
+++ src/Fish.cc
@@ -1361,6 +1361,10 @@ static FileInfo *ls_to_FileInfo(char *line)
       }
 
       fi->SetSize(size);
+      fi->SetUser(user);
+      fi->SetGroup(group);
+      fi->SetNlink(n_links);
+      fi->SetMode(perms_code);
 
       return fi;
    }
