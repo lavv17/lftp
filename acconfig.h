2341151d72da34593e511e27d35893dc27bbf6ae
lav <>
2002-01-22 08:59:55 +0000

    *** empty log message ***

--- acconfig.h
+++ acconfig.h
@@ -258,7 +258,7 @@ typedef unsigned _bool;
 #endif
 
 #ifdef __GNUC__
-# define PRINTF_LIKE(n,m) __attribute__((format(printf,n,m)))
+# define PRINTF_LIKE(n,m) __attribute__((format(__printf__,n,m)))
 #else
 # define PRINTF_LIKE(n,m)
 #endif
