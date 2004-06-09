f51ea0b0320720dc279def698b9653abcf82a413
lav <>
2004-06-09 09:44:15 +0000

    put'em in PD

--- src/plural.h
+++ src/plural.h
@@ -1,22 +1,12 @@
 /*
- * lftp and utils
+ * plural word form chooser for i18n
  *
  * Copyright (c) 1998 by Alexander V. Lukyanov (lav@yars.free.net)
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * This file is in public domain.
  */
+
+/* $Id$ */
 
 #ifndef PLURAL_H
 #define PLURAL_H
