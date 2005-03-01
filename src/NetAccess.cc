c9dae1eaf4f9c4a96aee09af277e7a424b184850
lav <>
2005-03-01 14:41:47 +0000

    * NetAccess.cc: make TOS changes compile on solaris.

--- src/NetAccess.cc
+++ src/NetAccess.cc
@@ -27,7 +27,12 @@
 #include <math.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <netinet/ip.h>
+#ifdef HAVE_NETINET_IN_SYSTM_H
+# include <netinet/in_systm.h>
+#endif
+#ifdef HAVE_NETINET_IP_H
+# include <netinet/ip.h>
+#endif
 #ifdef HAVE_NETINET_TCP_H
 # include <netinet/tcp.h>
 #endif
