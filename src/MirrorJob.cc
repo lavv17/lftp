67dc0a76a9bfe3f21ffd8bfe636fc9fac00572dd
lav <>
2005-06-29 10:23:24 +0000

    * MirrorJob.cc: workaround FreeBSD tcgetpgrp bug.

--- src/MirrorJob.cc
+++ src/MirrorJob.cc
@@ -1077,7 +1077,7 @@ void MirrorJob::va_Report(const char *fmt,va_list v)
    if(verbose_report)
    {
       pid_t p=tcgetpgrp(fileno(stdout));
-      if(p!=-1 && p!=getpgrp())
+      if(p>0 && p!=getpgrp())
 	 return;
 
       vfprintf(stdout,fmt,v);
