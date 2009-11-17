7c42d6f51519b31853bc0acc8298a17a21ec7fed
lav <>
2009-11-17 08:16:44 +0000

    2009-11-17  Robert Story <Robert.Story@cobham.com>
    	* Resolver.cc: use VAL_GETADDRINFO_HAS_STATUS macro.

--- src/Resolver.cc
+++ src/Resolver.cc
@@ -727,7 +727,8 @@ void Resolver::LookupOne(const char *name)
       bool require_trust=ResMgr::QueryBool("dns:strict-dnssec",name);
       ainfo_res	= val_getaddrinfo(NULL, name, NULL, &a_hint, &ainfo,
                                   &val_status);
-      if(ainfo && !val_istrusted(val_status) && require_trust) {
+      if(VAL_GETADDRINFO_HAS_STATUS(ainfo_res)
+      && !val_istrusted(val_status) && require_trust) {
           // untrusted answer
           error = _("DNS resoloution not trusted.");
           break;
