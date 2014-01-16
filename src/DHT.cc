36df8d4fa21d4a0eb0e2f6e125d8d44a46796e2c
Alexander V. Lukyanov <lav@yars.free.net>
2014-01-16 13:13:48 +0400

    copyright year update to 2014

--- src/DHT.cc
+++ src/DHT.cc
@@ -1,7 +1,7 @@
 /*
  * lftp - file transfer program
  *
- * Copyright (c) 2012-2013 by Alexander V. Lukyanov (lav@yars.free.net)
+ * Copyright (c) 2012-2014 by Alexander V. Lukyanov (lav@yars.free.net)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/commands.cc
+++ src/commands.cc
@@ -1,7 +1,7 @@
 /*
  * lftp - file transfer program
  *
- * Copyright (c) 1996-2013 by Alexander V. Lukyanov (lav@yars.free.net)
+ * Copyright (c) 1996-2014 by Alexander V. Lukyanov (lav@yars.free.net)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -2584,7 +2584,7 @@ CMD(help)
 CMD(ver)
 {
    printf(
-      _("LFTP | Version %s | Copyright (c) 1996-%d Alexander V. Lukyanov\n"),VERSION,2013);
+      _("LFTP | Version %s | Copyright (c) 1996-%d Alexander V. Lukyanov\n"),VERSION,2014);
    printf("\n");
    printf(
       _("LFTP is free software: you can redistribute it and/or modify\n"
