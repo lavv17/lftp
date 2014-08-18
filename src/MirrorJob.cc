d14542edb3bd498f06ddfddc6c7c9f4a82b1e3c0
Alexander V. Lukyanov <lavv17f@gmail.com>
2014-08-18 12:04:21 +0400

    mirror: report finished transfers (--verbose=2+).

--- src/MirrorJob.cc
+++ src/MirrorJob.cc
@@ -211,6 +211,18 @@ void MirrorJob::TransferFinished(Job *j)
    AddBytesTransferred(bytes_count);
    stats.bytes+=bytes_count;
    stats.time +=j->GetTimeSpent();
+   if(j->ExitCode()==0 && verbose_report>=2) {
+      xstring finished;
+      const xstring& cmd=j->GetCmdLine();
+      if(cmd[0]=='\\')
+	 finished.append(cmd+1,cmd.length()-1);
+      else
+	 finished.append(cmd);
+      const xstring& rate=Speedometer::GetStrProper(j->GetTransferRate());
+      if(rate.length()>0)
+	 finished.append(" (").append(rate).append(')');
+      Report(_("Finished %s"),finished.get());
+   }
    JobFinished(j);
    if(transfer_count==0)
       root_mirror->transfer_time_elapsed += now-root_mirror->transfer_start_ts;
