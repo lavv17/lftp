efd1d2fd8d9e540c1d07dcbb5713a684941c5bc6
Alexander V. Lukyanov <lavv17f@gmail.com>
2014-09-22 13:49:14 +0400

    buffer_zlib: try to inflate all passed data, save what cannot be inflated right now.
    This fixes an obscure inflation bug when the inflated data could not fit into size*6 store buffer.

--- src/buffer_zlib.cc
+++ src/buffer_zlib.cc
@@ -29,46 +29,59 @@ void DataInflator::PutTranslated(Buffer *target,const char *put_buf,int size)
       Get(&put_buf,&size);
       from_untranslated=true;
    }
-   if(size<=0)
-      return;
-   if(z_err==Z_STREAM_END)
+   // process all data we can, save the rest in the untranslated buffer
+   while(size>0)
    {
-      // assume the data after the compressed stream are not compressed.
-      target->Put(put_buf,size);
-      if(from_untranslated)
-	 Skip(size);
-      return;
-   }
-   size_t put_size=size;
+      if(z_err==Z_STREAM_END)
+      {
+	 // assume the data after the compressed stream are not compressed.
+	 target->Put(put_buf,size);
+	 if(from_untranslated)
+	    Skip(size);
+	 return;
+      }
+      size_t put_size=size;
+      int size_coeff=6;
+      size_t store_size=size_coeff*put_size;
+      char *store_space=target->GetSpace(store_size);
+      char *store_buf=store_space;
+      // do the inflation
+      z.next_in=(Bytef*)put_buf;
+      z.avail_in=put_size;
+      z.next_out=(Bytef*)store_buf;
+      z.avail_out=store_size;
+      int ret = inflate(&z, Z_NO_FLUSH);
+      assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+      switch (ret) {
+      case Z_NEED_DICT:
+	 ret = Z_DATA_ERROR;     /* and fall through */
+      case Z_DATA_ERROR:
+      case Z_MEM_ERROR:
+	 z_err=ret;
+	 target->SetError(xstring::cat("zlib inflate error: ",z.msg,NULL),true);
+	 return;
+      case Z_STREAM_END:
+	 z_err=ret;
+	 break;
+      }
+      int inflated_size=store_size-z.avail_out;
+      int processed_size=put_size-z.avail_in;
 
-   int size_coeff=6;
-   if(put_size==0)
-      return;
-   size_t store_size=size_coeff*put_size;
-   char *store_space=target->GetSpace(store_size);
-   char *store_buf=store_space;
-   // do the inflation
-   z.next_in=(Bytef*)put_buf;
-   z.avail_in=put_size;
-   z.next_out=(Bytef*)store_buf;
-   z.avail_out=store_size;
-   int ret = inflate(&z, Z_NO_FLUSH);
-   assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-   switch (ret) {
-   case Z_NEED_DICT:
-      ret = Z_DATA_ERROR;     /* and fall through */
-   case Z_DATA_ERROR:
-   case Z_MEM_ERROR:
-      z_err=ret;
-      target->SetError(xstring::cat("zlib inflate error: ",z.msg,NULL),true);
-      return;
-   case Z_STREAM_END:
-      z_err=ret;
-      break;
+      target->SpaceAdd(inflated_size);
+      if(from_untranslated) {
+	 Skip(processed_size);
+	 Get(&put_buf,&size);
+      } else {
+	 put_buf+=processed_size;
+	 size-=processed_size;
+      }
+      if(inflated_size==0) {
+	 // could not inflate any data, save unprocessed data
+	 if(!from_untranslated)
+	    Put(put_buf,size);
+	 return;
+      }
    }
-   target->SpaceAdd(store_size-z.avail_out);
-   if(from_untranslated)
-      Skip(put_size-z.avail_in);
 }
 
 DataInflator::DataInflator()
