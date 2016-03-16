b27aa108fa494e712422061b5bd7088922627623
Alexander V. Lukyanov <lavv17f@gmail.com>
2016-03-16 14:28:03 +0300

    update copyright year

--- src/commands.cc
+++ src/commands.cc
@@ -1,7 +1,7 @@
 /*
  * lftp - file transfer program
  *
- * Copyright (c) 1996-2015 by Alexander V. Lukyanov (lav@yars.free.net)
+ * Copyright (c) 1996-2016 by Alexander V. Lukyanov (lav@yars.free.net)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -2680,7 +2680,7 @@ CMD(help)
 CMD(ver)
 {
    printf(
-      _("LFTP | Version %s | Copyright (c) 1996-%d Alexander V. Lukyanov\n"),VERSION,2015);
+      _("LFTP | Version %s | Copyright (c) 1996-%d Alexander V. Lukyanov\n"),VERSION,2016);
    printf("\n");
    printf(
       _("LFTP is free software: you can redistribute it and/or modify\n"
