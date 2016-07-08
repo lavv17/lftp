d828ae6e38ad85863e403be8eb2f5b83b36baed9
tometzky <tometzky@ato.waw.pl>
2016-07-08 11:04:51 +0200

    mirror: don't set u+rwx on directories if already set (#253)
    * mirror: don't set u+rwx on directories if already set
    
    Running chmod will clear sgid permissions on directories when current user
    is not a member of directory group even when actually nothing is explicitly
    changed.
    
    * mirror: don't set u+rwx on new directories if already set
    
    Running chmod will clear sgid permissions on directories when current user
    is not a member of directory group even when actually nothing is explicitly
    changed.

--- src/MirrorJob.cc
+++ src/MirrorJob.cc
@@ -423,7 +423,11 @@ void  MirrorJob::HandleFile(FileInfo *file)
 	    {
 	       if(S_ISDIR(st.st_mode))
 	       {
-		  chmod(target_name,st.st_mode|0700);
+		  // try to enable write access
+		  // only if not enabled as chmod can clear sgid flags on directories
+		  if(st.st_mode!=(st.st_mode|0700)) {
+		    chmod(target_name,st.st_mode|0700);
+		  }
 		  create_target_dir=false;
 	       }
 	       else
@@ -815,7 +819,9 @@ int   MirrorJob::Do()
 	 {
 	    if(S_ISDIR(st.st_mode))
 	    {
-	       if(!script_only)
+	       // try to enable write access
+	       // only if not enabled as chmod can clear sgid flags on directories
+	       if(!script_only && (st.st_mode!=(st.st_mode|0700)))
 		  chmod(target_dir,st.st_mode|0700);
 	       create_target_dir=false;
 	       goto pre_CHANGING_DIR_TARGET;
