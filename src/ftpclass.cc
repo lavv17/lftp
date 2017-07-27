lavv17/lftp

commit 5419cd971d523c9fa523e37a26246bf582977f03

ftp: fixed a rare hang when a NOOP was sent between "transfer ok" reply and EOF on data socket.

src/ftpclass.cc
@@ -2566,6 +2566,12 @@ int   Ftp::Do()
       if(copy_mode!=COPY_NONE)
 	 goto notimeout_return;
 
+      if(expect->IsEmpty() && !eof)
+      {
+	 eof=true;
+	 m=MOVED;
+      }
+
       goto usual_return;
    }
    case WAITING_CCC_SHUTDOWN:

commit 30b26b5dc5e1b3ce0d0f6eedd9cdc09be880320f

ftpclass.cc: fixed QUOTE_CMD mode (broken in 5419cd971d523c9fa523e37a26246bf582977f03)

src/ftpclass.cc
@@ -2566,7 +2566,7 @@ int   Ftp::Do()
       if(copy_mode!=COPY_NONE)
 	 goto notimeout_return;
 
-      if(expect->IsEmpty() && !eof)
+      if(expect->IsEmpty() && !eof && !conn->data_iobuf)
       {
 	 eof=true;
 	 m=MOVED;
@@ -5040,7 +5040,7 @@ void TelnetEncode::PutTranslated(Buffer *target,const char *put_buf,int size)
    const char *iac;
    while(put_size>0)
    {
-      iac=(const char*)memchr(put_buf,TELNET_IAC,put_size);
+      iac=(const char*)memchr(put_buf,(unsigned char)TELNET_IAC,put_size);
       if(!iac)
       {
 	 target->Put(put_buf,put_size);
@@ -5066,7 +5066,7 @@ void TelnetDecode::PutTranslated(Buffer *target,const char *put_buf,int size)
    const char *iac;
    while(put_size>0)
    {
-      iac=(const char*)memchr(put_buf,TELNET_IAC,put_size);
+      iac=(const char*)memchr(put_buf,(unsigned char)TELNET_IAC,put_size);
       if(!iac)
 	 break;
       target->Put(put_buf,iac-put_buf);