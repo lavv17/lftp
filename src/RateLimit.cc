315eec549235c1514ba8aaaf6eb70e463f92bb67
Alexander V. Lukyanov <lavv17f@gmail.com>
2017-08-04 16:14:14 +0300

    RateLimit: allow per-host limits, allow closure for net:limit-total-{rate,max}
    RateLimit objects are now organized in three-level tree structure:
        TOTAL->PER_HOST->PER_CONN
    
    TOTAL is the root. No freeing of unused limits yet.

--- src/RateLimit.cc
+++ src/RateLimit.cc
@@ -22,24 +22,45 @@
 #include "ResMgr.h"
 #include "SMTask.h"
 
-// RateLimit class implementation.
-int RateLimit::total_xfer_number;
-RateLimit::BytesPool RateLimit::total[2];
-bool RateLimit::total_reconfig_needed=true;
+xmap_p<RateLimit> RateLimit::total;
 
-RateLimit::RateLimit(const char *c)
+void RateLimit::AddXfer(int add)
 {
-   if(total_xfer_number==0)
-   {
-      total[GET].Reset();
-      total[PUT].Reset();
-   }
-   total_xfer_number++;
+   xfer_number+=add;
+   assert(xfer_number>=0);
+   if(parent)
+      parent->AddXfer(add);
+}
+
+void RateLimit::init(level_e lvl,const char *c)
+{
+   level=lvl;
+   xfer_number=(level==PER_CONN?1:0);
+   parent=0;
    Reconfig(0,c);
+
+   if(level==TOTAL) // has no parent
+      return;
+
+   level_e parent_level=level_e(level+1);
+   if(parent_level==TOTAL)
+      c=""; // no closure on top level
+   xstring parent_key(c);
+
+   if(total.exists(parent_key)) {
+      parent=total.lookup(parent_key);
+      if(parent->xfer_number==0)
+	 parent->Reconfig(0,c); // it was not used for a white, refresh config
+   } else {
+      parent=new RateLimit(parent_level,c);
+      total.add(parent_key,parent);
+   }
+   parent->AddXfer(xfer_number);
 }
 RateLimit::~RateLimit()
 {
-   total_xfer_number--;
+   if(parent)
+      parent->AddXfer(-xfer_number);
 }
 
 #define LARGE 0x10000000
@@ -65,38 +86,32 @@ void RateLimit::BytesPool::AdjustTime()
 
 int RateLimit::BytesAllowed(dir_t dir)
 {
-   if(total_reconfig_needed)
-      ReconfigTotal();
+   int parent_allowed = parent?parent->BytesAllowed(dir):LARGE;
 
-   if(one[dir].rate==0 && total[dir].rate==0) // unlimited
-      return LARGE;
+   if(pool[dir].rate==0) // unlimited
+      return parent_allowed;
 
-   one  [dir].AdjustTime();
-   total[dir].AdjustTime();
+   pool[dir].AdjustTime();
 
-   int ret=LARGE;
-   if(total[dir].rate>0)
-      ret=total[dir].pool/total_xfer_number;
-   if(one[dir].rate>0 && ret>one[dir].pool)
-      ret=one[dir].pool;
-   return ret;
+   int allowed = pool[dir].pool/xfer_number;
+   if(allowed>parent_allowed)
+      allowed=parent_allowed;
+
+   return allowed;
 }
 
 bool RateLimit::Relaxed(dir_t dir)
 {
-   if(total_reconfig_needed)
-      ReconfigTotal();
+   bool parent_relaxed = parent?parent->Relaxed(dir):true;
 
-   if(one[dir].rate==0 && total[dir].rate==0) // unlimited
-      return true;
-   one  [dir].AdjustTime();
-   total[dir].AdjustTime();
+   if(pool[dir].rate==0) // unlimited
+      return parent_relaxed;
 
-   if(total[dir].rate>0 && total[dir].pool < total[dir].pool_max/2)
+   pool[dir].AdjustTime();
+
+   if(pool[dir].rate>0 && pool[dir].pool < pool[dir].pool_max/2)
       return false;
-   if(one[dir].rate>0 && one[dir].pool < one[dir].pool_max/2)
-      return false;
-   return true;
+   return parent_relaxed;
 }
 
 void RateLimit::BytesPool::Used(int bytes)
@@ -109,8 +124,15 @@ void RateLimit::BytesPool::Used(int bytes)
 
 void RateLimit::BytesUsed(int bytes,dir_t dir)
 {
-   total[dir].Used(bytes);
-   one  [dir].Used(bytes);
+   if(parent)
+      parent->BytesUsed(bytes,dir);
+   pool[dir].Used(bytes);
+}
+
+void RateLimit::Reset()
+{
+   pool[GET].Reset();
+   pool[PUT].Reset();
 }
 
 void RateLimit::BytesPool::Reset()
@@ -121,36 +143,41 @@ void RateLimit::BytesPool::Reset()
 void RateLimit::Reconfig(const char *name,const char *c)
 {
    if(name && strncmp(name,"net:limit-",10))
-      return;
-   ResMgr::Query("net:limit-rate",c).ToNumberPair(one[GET].rate,one[PUT].rate);
-   ResMgr::Query("net:limit-max",c) .ToNumberPair(one[GET].pool_max,one[PUT].pool_max);
-   if(one[GET].pool_max==0)
-      one[GET].pool_max=one[GET].rate*DEFAULT_MAX_COEFF;
-   if(one[PUT].pool_max==0)
-      one[PUT].pool_max=one[PUT].rate*DEFAULT_MAX_COEFF;
-   one[GET].Reset(); // to cut bytes_pool.
-   one[PUT].Reset();
+      return; // not relevant
 
-   if(name && !strncmp(name,"net:limit-total-",16))
-      total_reconfig_needed=true;
-}
-void RateLimit::ReconfigTotal()
-{
-   ResMgr::Query("net:limit-total-rate",0).ToNumberPair(total[GET].rate,total[PUT].rate);
-   ResMgr::Query("net:limit-total-max",0) .ToNumberPair(total[GET].pool_max,total[PUT].pool_max);
-   if(total[GET].pool_max==0)
-      total[GET].pool_max=total[GET].rate*DEFAULT_MAX_COEFF;
-   if(total[PUT].pool_max==0)
-      total[PUT].pool_max=total[PUT].rate*DEFAULT_MAX_COEFF;
-   total[GET].Reset();
-   total[PUT].Reset();
-   total_reconfig_needed = false;
+   bool config_total=(!name || !strncmp(name,"net:limit-total-",16));
+
+   const char *setting_rate="net:limit-rate";
+   const char *setting_max="net:limit-max";
+
+   if(level>PER_CONN)
+   {
+      if(!config_total)
+	 return; // not relevant
+      if(level==TOTAL)
+	 c=0; // aggregates everything
+
+      setting_rate="net:limit-total-rate";
+      setting_max="net:limit-total-max";
+   }
+
+   ResMgr::Query(setting_rate,c).ToNumberPair(pool[GET].rate,pool[PUT].rate);
+   ResMgr::Query(setting_max,c).ToNumberPair(pool[GET].pool_max,pool[PUT].pool_max);
+
+   if(pool[GET].pool_max==0)
+      pool[GET].pool_max=pool[GET].rate*DEFAULT_MAX_COEFF;
+   if(pool[PUT].pool_max==0)
+      pool[PUT].pool_max=pool[PUT].rate*DEFAULT_MAX_COEFF;
+   Reset();
+
+   if(config_total && parent)
+      parent->Reconfig(name,c);
 }
 
 int RateLimit::LimitBufferSize(int size,dir_t d) const
 {
-   if(one[d].rate!=0 && size>one[d].pool_max)
-      size=one[d].pool_max;
+   if(pool[d].rate!=0 && size>pool[d].pool_max)
+      size=pool[d].pool_max;
    return size;
 }
 void RateLimit::SetBufferSize(IOBuffer *buf,int size) const
